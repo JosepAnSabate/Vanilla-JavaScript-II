23952de9b23668e9f3ae8401c54f010584e80c1b
josep.andreu@e-campus.uab.cat <josep.andreu@e-campus.uab.cat>
2021-09-24 00:39:40 +0200

    finished-blackjack

--- blackjack-app2/index.js
+++ blackjack-app2/index.js
@@ -1,21 +1,78 @@
-let firstCard= 8
-let secondCard= 1
-let sum = firstCard+secondCard
+
+let player= {  //object to store data
+    name: "Pep",
+    chips: 150,
+    sayHello: function() {
+        console.log("Welcome!")
+    }
+}
+player.sayHello()
+
+let sum = 0
+let cards= []
 
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 
 let message = ""
 
-if (sum < 21) {
-    message = (sum + " Do you want to draw a new card?")
-} else if (sum === 21) {
-    message=(sum + " Blackjack!")
-    hasBlackJack=true
-} else {
-    message=(sum + " Game Over!")
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl=document.getElementById("cards-el")
+
+// render the player's name and chip
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": " + player.chips + "€"
+
+function startGame() {
+    isAlive = true
+    let firstCard= getRandomCard()
+    let secondCard= getRandomCard()
+    cards.push(firstCard)
+    cards.push(secondCard)
+    sum = firstCard+secondCard
+    renderGame()
 }
 
-console.log(message)
-console.log(isAlive)
+function renderGame() { 
+    cardsEl.textContent= "Cards: "
+    for(let i =0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    console.log(sum)
+    sumEl.textContent = "Sum: " + sum
+    if (sum < 21) {
+        message = ("Do you want to draw a new card?")
+    } else if (sum === 21) {
+        message=("Blackjack!")
+        hasBlackJack=true
+    } else {
+        message=("Game Over!")
+        isAlive = false
+    }
+    messageEl.textContent = message
+    
+}
+
+function anotherCard() {
+    if (isAlive === true && hasBlackJack === false) {
+    console.log("Drawing a new card") 
+    let newCard = getRandomCard()
+    sum += newCard
+    cards.push(newCard)
+    renderGame()   
+    }
+}
+
+function getRandomCard() {
+    let random = Math.floor(Math.random()*13) +1
+    if (random === 1) {
+        return 11
+    } else if (random > 10 ){
+        return 10
+    } else {
+        return random
+    }
+    //Math.random() used with Math.floor() can be used to return random integers.
+}
+
--- u2/index.js
+++ u2/index.js
@@ -6,7 +6,7 @@
 //     console.log("Welcome!")
 // }
 
-// ex king norway
+// condicionals
 // let age = 100
 
 // if (age < 100) {
@@ -17,4 +17,124 @@
 //     console.log("You have already gotten one")
 // }
 
-//
+// for loops
+// for (let count=1; count < 11; count += 1) {
+//     console.log(count)
+// }
+// for (let i = 10; i <= 100; i+=10) {
+//     console.log(i)
+// }
+
+// let message = ["a","b","c","d"]
+
+// for (let i= 0; i < 5; i += 1){
+//     console.log(message[i ])
+// }
+
+
+// function roll(){
+//     let random = Math.floor(Math.random() * 12) + 1; // Math.random() used with Math.floor() can be used to return random integers.
+//     return random
+// }
+// console.log(
+// roll())
+
+// let course = {
+//     title: "Learn CSS",
+//     lessons: 16,
+//     creator: "Per",
+//     length: 63,
+//     level:2,
+//     isFree: true,
+//     tags: ["html", "css"]
+// }
+
+// console.log(course["tags"])
+
+
+// let castle = {
+//     title: "Castell riudsabella",
+//     piscina: true,
+//     habitacions: 9,
+//     conreus: ["img/vinya.jpg", "vinya"]
+// }
+
+// console.log(castle.conreus[0])
+
+// let person = {
+//     name: "Josep",
+//     age: 26,
+//     country: "Catalunya"
+// }
+
+// function logData(){
+//     let data = person.name +" is " + person.age + "  years old and lives in " + person.country
+//     console.log(data)
+// }
+
+// logData()
+
+
+//condicionals
+// let age = 15
+
+// if (age < 6){
+//     console.log("free")
+// } else if( age <= 17){
+//     console.log("child discount")
+// } else if(age <= 26) {
+//     console.log("student discount")
+// } else if(age <=66){
+//     console.log("full price")
+// } else {
+//     console.log(" senior discount")
+// }   
+
+// for loop
+// let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistant"]
+
+// for(let i=0; i < largeCountries.length; i++){
+//     console.log("- " + largeCountries[i])
+// }
+
+// let dayOfMonth = 13
+// let weekday = "Friday"
+
+// if ( dayOfMonth === 13 && weekday === "Friday"){
+//     console.log("RUN")
+// }
+
+// rock paper scissor
+// let hands = ["rock", "paper", "scissor"]
+
+// function partida() {
+//     let random = Math.floor(Math.random() * 3);
+//     let resultat = hands[random]
+//     console.log("Winner: "+resultat)
+// }
+
+// partida()
+
+let fruits = ["apple", "orange", "apple", "apple", "orange"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+
+
+function sortFruit() {
+    for (let i = 0; i < fruits.length; i++) {
+        apples = 0
+        oranges = 0
+        if (fruits[i] === "apple") {
+            appleShelf.textContent += "apple + "
+        } else {
+            orangeShelf.textContent += "oranges + "
+        }
+    }
+}
+
+sortFruit()
+
+
+
+
